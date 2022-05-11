601ba330d0f58eb83a5456d98efa77466e6cf32f
z <123>
2022-05-12 05:49:15 +0800

    changed: advanced smeltery - energy 240 to 440

--- src/main/java/io/github/mooy1/infinityexpansion/items/SlimefunExtension.java
+++ src/main/java/io/github/mooy1/infinityexpansion/items/SlimefunExtension.java
@@ -62,7 +62,7 @@ public final class SlimefunExtension {
     private static final int INFINITY_CHARGER_SPEED = 6000;
     private static final int INFINITY_CHARGER_ENERGY = 60000;
 
-    private static final int ADVANCED_SMELTERY_ENERGY = 240;
+    private static final int ADVANCED_SMELTERY_ENERGY = 440;
     private static final int ADVANCED_SMELTERY_SPEED = 72;
 
     private static final int STAR_ENERGY = 1800;
